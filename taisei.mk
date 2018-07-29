4acc9a409cf8c90b7bf6aea2f2c6cdf8c30880e1
Andrei Alexeyev <0x416b617269@gmail.com>
2018-07-29 16:07:16 +0300

    taisei: switch to gcc8

--- taisei.mk
+++ taisei.mk
@@ -1,6 +1,6 @@
 
 
-override MXE_PLUGIN_DIRS += plugins/gcc7 plugins/meson-wrapper plugins/freetype-no-harfbuzz plugins/sdl2_image-lean
+override MXE_PLUGIN_DIRS += plugins/gcc8 plugins/meson-wrapper plugins/freetype-no-harfbuzz plugins/sdl2_image-lean
 
 MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
 LOCAL_PKG_LIST := gcc sdl2 sdl2_mixer sdl2_ttf sdl2_image zlib libpng libzip meson-wrapper
