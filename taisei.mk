e1348753566b993d335bba795124a8f85359c55a
Andrei Alexeyev <0x416b617269@gmail.com>
2018-08-02 13:04:00 +0300

    Don't build sdl2_ttf by default, taisei doesn't require it anymore

--- taisei.mk
+++ taisei.mk
@@ -3,6 +3,6 @@
 override MXE_PLUGIN_DIRS += plugins/gcc8 plugins/meson-wrapper plugins/freetype-no-harfbuzz plugins/sdl2_image-lean
 
 MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
-LOCAL_PKG_LIST := gcc sdl2 sdl2_mixer sdl2_ttf sdl2_image zlib libpng libzip meson-wrapper
+LOCAL_PKG_LIST := sdl2 sdl2_mixer sdl2_image zlib libpng libzip meson-wrapper
 .DEFAULT local-pkg-list:
 local-pkg-list: $(LOCAL_PKG_LIST)
