385a667d48bedbde5b831ce8805446daaaf7b372
Andrei Alexeyev <0x416b617269@gmail.com>
2018-09-24 18:27:47 +0300

    Reformat taisei.mk and build shaderc by default

--- taisei.mk
+++ taisei.mk
@@ -1,8 +1,24 @@
 
+TAISEI_PLUGINS := \
+	plugins/freetype-no-harfbuzz \
+	plugins/gcc8 \
+	plugins/meson-wrapper \
+	plugins/sdl2_image-lean \
 
-override MXE_PLUGIN_DIRS += plugins/gcc8 plugins/meson-wrapper plugins/freetype-no-harfbuzz plugins/sdl2_image-lean
+TAISEI_TARGETS := \
+	i686-w64-mingw32.static \
+	x86_64-w64-mingw32.static \
 
-MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
-LOCAL_PKG_LIST := sdl2 sdl2_mixer sdl2_image zlib libpng libzip meson-wrapper freetype
+TAISEI_PACKAGES := \
+	freetype \
+	libpng libzip \
+	meson-wrapper \
+	sdl2 sdl2_mixer \
+	sdl2_image zlib \
+	shaderc \
+
+override MXE_PLUGIN_DIRS += $(TAISEI_PLUGINS)
+MXE_TARGETS := $(TAISEI_TARGETS)
+LOCAL_PKG_LIST := $(TAISEI_PACKAGES)
 .DEFAULT local-pkg-list:
 local-pkg-list: $(LOCAL_PKG_LIST)
